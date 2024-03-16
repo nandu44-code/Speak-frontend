6835be5e48cb9d3964923a5637f645682fc96da8
nandu44-code <nanduganeshnair4444@gmail.com>
2024-03-16 14:30:29 +0530

    UI fix

--- Frontend/src/components/tutor/CreateSlotForm.jsx
+++ Frontend/src/components/tutor/CreateSlotForm.jsx
@@ -1,25 +1,42 @@
-// CreateSlotForm.jsx
-import React, { useState } from 'react';
-import api from '../../services/Axios';
-
+import React, { useState, useEffect } from "react";
+import api from "../../services/Axios";
+import { jwtDecode } from "jwt-decode";
+import { toast } from "react-toastify";
 function CreateSlotForm() {
   const [formData, setFormData] = useState({
-    date: '',
-    startTime: '',
-    endTime: ''
+    tutor: "",
+    date: "",
+    start_time: "",
+    end_time: "",
   });
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    const access = jwtDecode(token);
+
+    if (access.is_tutor) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        tutor: access.tutor,
+      }));
+    }
+  }, []);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData)
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      await api.post('/api/slots/', formData);
-      // Optional: Redirect to slot list page or show success message
-    } catch (error) {
-      console.error('Error creating slot:', error);
+
+   
+      try {
+        await api.post("tutor/slots/", formData);
+        // Optional: Redirect to slot list page or show success message
+        toast.success('slot created successfully')
+      } catch (error) {
+        console.error("Error creating slot:", error);
     }
   };
 
@@ -28,7 +45,9 @@ function CreateSlotForm() {
       <h2 className="text-xl font-bold mb-4">Create Slot</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label htmlFor="date" className="block text-gray-700 font-bold mb-2">Date</label>
+          <label htmlFor="date" className="block text-gray-700 font-bold mb-2">
+            Date
+          </label>
           <input
             type="date"
             id="date"
@@ -40,30 +59,43 @@ function CreateSlotForm() {
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="startTime" className="block text-gray-700 font-bold mb-2">Start Time</label>
+          <label
+            htmlFor="startTime"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Start Time
+          </label>
           <input
             type="time"
             id="startTime"
-            name="startTime"
-            value={formData.startTime}
+            name="start_time"
+            value={formData.start_time}
             onChange={handleChange}
             className="border border-gray-300 rounded-md p-2 w-full"
             required
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="endTime" className="block text-gray-700 font-bold mb-2">End Time</label>
+          <label
+            htmlFor="endTime"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            End Time
+          </label>
           <input
             type="time"
-            id="endTime"
-            name="endTime"
-            value={formData.endTime}
+            id="end_time"
+            name="end_time"
+            value={formData.end_time}
             onChange={handleChange}
             className="border border-gray-300 rounded-md p-2 w-full"
             required
           />
         </div>
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
           Create Slot
         </button>
       </form>
---
+++ Frontend/src/pages/Student/TutorDetailsPage.jsx
@@ -0,0 +1,124 @@
+import React, { useDebugValue, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import api from "../../services/Axios";
+import { useDispatch } from "react-redux";
+
+function TutorDetailsPage() {
+  const { id } = useParams();
+  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get(`user/${id}/`);
+        setUserData(response.data);
+      } catch (error) {
+        console.error();
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  const handleApproval = async () => {
+    const credentials = {
+      is_approved: true,
+    };
+    try {
+      const response = await api.patch(`users/${id}/`, credentials);
+      navigate("/admin/requests");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleReject = async () => {
+    const credentials = {
+      is_rejected: true,
+    };
+    try {
+      const response = await api.patch(`users/${id}/`, credentials);
+      navigate("/admin/requests");
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center m-10 bg-stone-200 rounded-3xl p-10">
+      {userData && (
+        <img
+          className="w-40 h-40 cursor-pointer mb-6"
+          src={userData.profile_image}
+          alt="Profile"
+        />
+      )}
+
+      <div className="bg-slate-100 p-6 rounded-xl w-full mb-6">
+        {/* <p className="font-bold text-2xl text-gray-500">Basic Details</p> */}
+        <div className="flex flex-row justify-center mb-2">
+    
+          <p className="font-bold text-2xl text-violet-900 ml-2">
+            {userData && userData.first_name}
+            <span className="mx-1">&nbsp;</span>
+            {userData && userData.last_name}
+          </p>
+        </div>
+
+        <p className="font-bold text-2xl text-violet-900 mb-2">
+          {userData && userData.email}
+        </p>
+
+        <div className="flex justify-center">
+          <p className="font-semibold text-xl mr-4">
+            {userData && userData.tutor.state}
+          </p>
+          <p className="font-semibold text-xl">
+            {userData && userData.tutor.country}
+          </p>
+        </div>
+      </div>
+
+      {userData && (
+        <div>
+        <div className="bg-gray-100 rounded-xl p-4 m-2">
+          <p className="font-bold text-2xl text-violet-900 mb-4">
+            {userData.username}
+          </p>
+          <p className="font-normal mb-4">
+            {userData.tutor.introduction_description}
+          </p>
+        </div>
+        <div className="bg-gray-100 rounded-xl p-4">
+          <label className="font-bold text-xl mb-2">Teaching style</label>
+          <p className="font-thintext-2xl mb-4">
+            {userData.tutor.teaching_style}
+          </p>
+          {/* <img
+            src={userData.tutor.certificates}
+            className="w-60 h-20 cursor-pointer mb-4"
+            alt="Certificates"
+          /> */}
+        </div>
+      </div>
+      )}
+
+      <div className="flex justify-evenly w-full">
+        <button
+          className="bg-gradient-to-br from-purple-600 to-blue-500 text-white py-2 px-8 rounded-md hover:bg-opacity-0 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          onClick={handleApproval}
+        >
+          Accept
+        </button>
+        <button
+          className="bg-gradient-to-br from-purple-600 to-blue-500 text-white py-2 px-8 rounded-md hover:bg-opacity-0"
+          onClick={handleReject}
+        >
+          Reject
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TutorDetailsPage;
