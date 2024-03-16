32215c140b277d4f28a62dcd445a66481eef4b11
nandu44-code <nanduganeshnair4444@gmail.com>
2024-03-16 14:58:57 +0530

    UI fix on tutor card

--- Frontend/src/pages/Student/FindTutorsPage.jsx
+++ Frontend/src/pages/Student/FindTutorsPage.jsx
@@ -29,9 +29,9 @@ import Navbar from '../../components/Navbar';
         <>
         <Navbar/>
         <p className='font-bold text-2xl text-indigo-900 mt-10 select-none'>Find a Tutor</p>
-        <input type='search' placeholder='search...' className='rounded-full placeholder-indigo-950 placeholder:font-bold w-96 px-4 py-2 my-10 focus:bg-gray-200 
+        <input type='search' placeholder='search...' className='rounded-full 2xl:w-96 w-56 placeholder-indigo-950 placeholder:font-bold px-4 py-2 my-10 focus:bg-gray-200 
          bg-stone-200'/>
-        <div className='flex justify-evenly'>
+        <div className='flex flex-wrap justify-center items-center sm:flex-row md:flex-row lg:flex-row xl:flex-row 2xl:flex-row '>
         {tutors.map((user) => (
                 <TutorCard 
                 key={user.id}
