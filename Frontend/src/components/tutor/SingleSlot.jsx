af6f8faf6e5b61bbf88ed69a992e5406d111abeb
nandu44-code <nanduganeshnair4444@gmail.com>
2024-03-28 00:40:32 +0530

    UI changes at Singleslot component

--- Frontend/src/components/tutor/SingleSlot.jsx
+++ Frontend/src/components/tutor/SingleSlot.jsx
@@ -7,23 +7,34 @@ const SingleSlot = ({ startDate, startTime, endTime, onDelete }) => {
     onDelete();
   };
 
+  const handleBooking = () => {
+    console.log("booking ")
+  }
+
   const location = useLocation();
   console.log(location.pathname);
   return (
-    <div className="border border-gray-300 rounded-lg p-4 mb-4 flex justify-between items-center hover:scale-95 duration-1000 hover:bg-stone-200 hover:cursor-pointer">
+    <div className="flex justify-center items-center">
+    <div className="border w-1/2 border-gray-300 rounded-lg p-4 mb-4 flex justify-between items-center duration-1000 hover:bg-stone-200 hover:cursor-pointer">
       <div>
         <p className="text-Indigo-800 font-bold">Date: {startDate}</p>
         <p className="text-blue-700 font-semibold">Start Time: {startTime}</p>
         <p className="text-red-700 font-semibold">End Time: {endTime}</p>
       </div>
-      {location.pathname == "/tutor/viewslot/" && (
+      {location.pathname == "/tutor/viewslot/" ? (
         <button
           onClick={handleDelete}
           className="border-2 border-red-500 text-red-900 px-4 py-2 rounded-md hover:bg-red-400 hover:text-white hover:cursor-pointer"
         >
           Delete
         </button>
+      ) : (
+        <button className="border-2 border-red-500 text-red-900 px-4 py-2 rounded-md hover:bg-red-600 hover:text-white hover:cursor-pointer
+        " onClick={handleBooking}>
+          Book
+        </button>
       )}
+    </div>
     </div>
   );
 };
--- Frontend/src/pages/Student/TutorDetailsPage.jsx
+++ Frontend/src/pages/Student/TutorDetailsPage.jsx
@@ -3,11 +3,19 @@ import { useNavigate, useParams } from "react-router-dom";
 import api from "../../services/Axios";
 import { useDispatch } from "react-redux";
 import SingleSlot from "../../components/tutor/SingleSlot";
+import Navbar from "../../components/Navbar";
+import { AiOutlineMessage } from "react-icons/ai";
+import { RiUserFollowLine } from "react-icons/ri";
+import { BiHide } from "react-icons/bi";
+import { GrSchedule } from "react-icons/gr";
 
 function TutorSideDetailsPage() {
   const { id } = useParams();
   const [userData, setUserData] = useState(null);
-  const [selectedDate, setSelectedDate] = useState("");
+  // const currentdate = new Date();
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
   const [user, setUser] = useState("");
   const [slots, setSlots] = useState([]);
 
@@ -37,7 +45,6 @@ function TutorSideDetailsPage() {
     console.log(slots);
   }, [slots]);
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -50,106 +57,134 @@ function TutorSideDetailsPage() {
     fetchData();
   }, [id]);
 
-
   return (
-    <div className="flex flex-col items-center m-10 bg-stone-200 rounded-3xl p-10">
-      {userData && (
-        <img
-          className="w-40 h-40 cursor-pointer mb-6"
-          src={userData.profile_image}
-          alt="Profile"
-        />
-      )}
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center m-10 bg-gray-100  p-10 shadow-sm shadow-black">
+        {userData && (
+          <img
+            className="w-40 h-40 cursor-pointer mb-6"
+            src={userData.profile_image}
+            alt="Profile"
+          />
+        )}
 
-      <div className="bg-slate-100 p-6 rounded-xl w-full mb-6">
-        {/* <p className="font-bold text-2xl text-gray-500">Basic Details</p> */}
-        <div className="flex flex-row justify-center mb-2">
-    
-          <p className="font-bold text-2xl text-violet-900 ml-2">
-            {userData && userData.first_name}
-            <span className="mx-1">&nbsp;</span>
-            {userData && userData.last_name}
-          </p>
-        </div>
-
-        <p className="font-bold text-2xl text-violet-900 mb-2">
-          {userData && userData.email}
-        </p>
-
-        <div className="flex justify-center">
-          <p className="font-semibold text-xl mr-4">
-            {userData && userData.tutor.state}
-          </p>
-          <p className="font-semibold text-xl">
-            {userData && userData.tutor.country}
-          </p>
-        </div>
-      </div>
-
-      {userData && (
-        <div>
-        <div className="bg-gray-100 rounded-xl p-4 m-2">
-          <p className="font-bold text-2xl text-violet-900 mb-4">
-            {userData.username}
-          </p>
-          <p className="font-normal mb-4">
-            {userData.tutor.introduction_description}
-          </p>
-        </div>
-        <div className="bg-gray-100 rounded-xl p-4">
-          <label className="font-bold text-xl mb-2">Teaching style</label>
-          <p className="font-thintext-2xl mb-4">
-            {userData.tutor.teaching_style}
-          </p>
-        </div>
-      </div>
-      )}
-     <div className="w-auto ml-80 bg-stone-200 h-28 rounded-2xl px-4 py-2 mt-32">
-            {/* <h2 className="text-xl font-bold mb-4">Search Slots by Date</h2> */}
-            <form onSubmit={handleSubmit} className="flex items-center">
-              {" "}
-              {/* Added flex and items-center class */}
-              <div className="mb-4">
-                <label
-                  htmlFor="selectedDate"
-                  className="block text-gray-700 font-bold mb-2"
-                >
-                  Select Date
-                </label>
-                <input
-                  type="date"
-                  id="selectedDate"
-                  name="selectedDate"
-                  value={selectedDate}
-                  onChange={handleChange}
-                  className="border border-gray-300 rounded-md p-2 w-full"
-                  required
-                />
-              </div>
-              <button
-                type="submit"
-                className="bg-indigo-900 w-80 text-white px-4 py-2 mt-4 rounded-md hover:bg-indigo-600 ml-2"
-              >
-                Search Slots
-              </button>
-            </form>
+        <div className="bg-slate-100 p-6 rounded-xl w-full mb-6">
+          {/* <p className="font-bold text-2xl text-gray-500">Basic Details</p> */}
+          <div className="flex flex-row justify-center mb-2">
+            <p className="font-bold text-2xl text-violet-900 ml-2">
+              {userData && userData.first_name}
+              <span className="mx-1">&nbsp;</span>
+              {userData && userData.last_name}
+            </p>
           </div>
-          <div className="ml-96 mt-8">
-          {slots.length > 0 ? (
-            slots.map((slot) => (
-              <SingleSlot
-                key={slot.id}
-                startDate={slot.start_date}
-                startTime={slot.start_time}
-                endTime={slot.end_time}
-                onDelete={() => handleDeleteSlot(slot.id)}
-              />
-            ))
-          ) : (
-            <p>No results found</p>
-          )}
+
+          <p className="font-bold text-2xl text-violet-900 mb-2">
+            {userData && userData.email}
+          </p>
+
+          <div className="flex justify-center">
+            <p className="font-semibold text-xl mr-4">
+              {userData && userData.tutor.state}
+            </p>
+            <p className="font-semibold text-xl">
+              {userData && userData.tutor.country}
+            </p>
+          </div>
         </div>
-    </div>
+        <a href="#slots" className="w-full flex justify-center">
+          <button className="flex justify-center bg-white text-black font-medium text-lg px-4 py-2 m-2 w-1/2 border-2 border-blue-600 rounded-md hover:border-2 hover:border-indigo-400 hover:text-black hover:bg-gray-200">
+            <GrSchedule className="mt-1 mx-4" />
+            Schedule
+          </button>
+        </a>
+        <div className="flex w-1/2 justify-around my-5 sm:flex-row md:flex-col lg:flex-col xl:flex-row 2xl:flex-row">
+          <button className="flex bg-indigo-900 text-white px-4 py-1 my-3 rounded-md font-semibold hover:bg-indigo-700 duration-1000">
+            <RiUserFollowLine className="mt-1 mx-5" size={22} />
+            Follow
+          </button>
+          <button className="flex bg-indigo-900 text-white px-4 py-1 my-3 rounded-md font-semibold hover:bg-indigo-700 duration-1000">
+            {" "}
+            <AiOutlineMessage className="mt-1 mx-5" size={22} />
+            Message
+          </button>
+          <button className="flex bg-indigo-900 text-white px-4 py-1 my-3 rounded-md font-semibold hover:bg-indigo-700 duration-1000">
+            <BiHide className="mt-1 mx-5" size={22} />
+            Hide tutor
+          </button>
+        </div>
+
+        {userData && (
+          <div>
+            <div className="bg-gray-100 rounded-xl p-4 m-2">
+              <p className="font-bold text-2xl text-violet-900 mb-4">
+                {userData.username}
+              </p>
+              <p className="font-normal mb-4">
+                {userData.tutor.introduction_description}
+              </p>
+            </div>
+            <div className="bg-gray-100 rounded-xl p-4">
+              <label className="font-bold text-xl mb-2">Teaching style</label>
+              <p className="font-thintext-2xl mb-4">
+                {userData.tutor.teaching_style}
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="flex justify-center items-center">
+        <div
+          className="w-1/2 mx-4  flex justify-center items-center bg-gray-600 h-28 rounded-2xl px-4 py-2 my-20"
+          id="slots"
+        >
+          {/* <h2 className="text-xl font-bold mb-4">Search Slots by Date</h2> */}
+          <form onSubmit={handleSubmit} className="flex items-center">
+            {" "}
+            {/* Added flex and items-center class */}
+            <div className="mb-4">
+              <label
+                htmlFor="selectedDate"
+                className="block text-white font-bold mb-2 cursor-pointer"
+              >
+                Select Date
+              </label>
+              <input
+                type="date"
+                id="selectedDate"
+                name="selectedDate"
+                value={selectedDate}
+                onChange={handleChange}
+                className="border border-gray-300 rounded-md p-2 w-full"
+                default={selectedDate}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-indigo-900 w-80 text-white px-4 py-2 mt-4 rounded-md hover:bg-indigo-600 ml-2"
+            >
+              Search Slots
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="mb-10">
+        {slots.length > 0 ? (
+          slots.map((slot) => (
+            <SingleSlot
+              key={slot.id}
+              startDate={slot.start_date}
+              startTime={slot.start_time}
+              endTime={slot.end_time}
+              onDelete={() => handleDeleteSlot(slot.id)}
+            />
+          ))
+        ) : (
+          <p>No results found</p>
+        )}
+      </div>
+    </>
   );
 }
 
