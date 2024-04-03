074c39289ac460d2926bd22c6aaee2887c33792b
nandu44-code <nanduganeshnair4444@gmail.com>
2024-04-03 13:16:20 +0530

    UI changes at TutorCard

--- Frontend/src/components/TutorCard.jsx
+++ Frontend/src/components/TutorCard.jsx
@@ -4,7 +4,7 @@ function TutorCard(props) {
     const { name, country, state, imageUrl,onDetail } = props;
 
   return (
-   <div className="w-72 rounded-lg overflow-hidden bg-indigo-100 shadow-lg mx-6 my-10 cursor-pointer hover:scale-105 duration-700 hover:shadow-2xl" onClick={onDetail}>
+   <div className="w-72 rounded-lg overflow-hidden bg-stone-200 shadow-md mx-6 my-10 cursor-pointer hover:scale-105 duration-700 hover:shadow-2xl hover:shadow-neutral-200 hover:bg-white" onClick={onDetail}>
       <img className="w-full" src={imageUrl} alt="Tutor" />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
