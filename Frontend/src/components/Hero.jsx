e30802cd386a227bc566b2083bf85d664dfac551
nandu44-code <nanduganeshnair4444@gmail.com>
2024-04-05 15:49:16 +0530

    UI changes at hero section

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import imageSrc from '../assets/images/pexels-andrea-piacquadio-3772511.jpg';
+import heroimage1 from '../assets/images/heroimage1.jpg'
+import heroimage2 from '../assets/images/heroimage2.jpg'
 
 function Hero() {
   const backgroundImageUrl = '/assets/images/pexels-andrea-piacquadio-3772511.jpg';
 
   return (
-<div className="relative bg-cover bg-center flex flex-col items-center justify-center mt-0 w-full h-min opacity-95">
-  <div className="relative">
-    <img src={imageSrc} alt="Description of the image" className="rounded-sm shadow-2xl w-full h-auto" />
-
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-30 p-4 sm:mr-0 sm:mb-4 md:mr-10 md:mb-6 lg:mr-16 xl:mr-20 xl:mb-8">
-      <h1 className="font-semibold text-indigo-950 text-3xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl select-none">Online English Tutor</h1>
-      <p className="mt-2 text-gray-800 hidden sm:block md:block lg:block xl:block 2xl:block text-sm sm:text-md md:text-lg lg:text-base xl:text-xl select-none">Private online sessions with native English speakers</p>
-      <p className="mt-6 text-gray-700 text-sm hidden sm:text-lg md:text-xl lg:text-lg xl:text-xl select-none">Book your online English tutoring session today and improve your language skills from the comfort of your home.</p>
-      {/* <button className="mt-8 ml-28 bg-indigo-900 hover:bg-indigo-600 sm:block md:block lg:block xl:block 2xl:block hidden text-white py-3 px-6 rounded-full font-semibold text-lg focus:outline-none">Book Now</button> */}
+<div className="relative bg-cover bg-center flex flex-row-reverse items-center justify-center mt-0 w-full h-min opacity-95">
+  <div className="relative flex flex-col justify-center items-center w-full h-full lg:w-1/2">
+    <h1 className="font-semibold text-indigo-900 text-5xl sm:text-6xl md:text-7xl lg:text-7xl xl:text-7xl select-none">Online English Tutor</h1>
+    <p className="mt-2 text-gray-800 text-sm sm:text-md md:text-lg lg:text-base xl:text-xl select-none">Private online sessions with native English speakers</p>
+    {/* <button className="mt-8 bg-indigo-900 hover:bg-indigo-600 text-white py-3 px-6 rounded-full font-semibold text-lg focus:outline-none">Book Now</button> */}
+  </div>
+  <div className="flex flex-col justify-between w-full h-full lg:w-1/2 lg:flex-row lg:flex-wrap">
+    <div className="relative w-1/3 h-1/3 m-4">
+      <img src={imageSrc} alt="Description of the image" className="rounded-sm shadow-2xl w-full h-full" />
     </div>
-
+    <div className="relative w-1/2 h-1/2 m-2 rounded-lg">
+      <img src={heroimage1} alt="Description of the image" className="rounded-sm shadow-2xl w-full h-full" />
+    </div>
+    <div className="relative w-1/2 h-1/2 m-4">
+      <img src={heroimage2} alt="Description of the image" className="rounded-sm shadow-2xl w-full h-1/2" />
+    </div>
+    <div className="relative w-1/3 h-1/2 m-4">
+      <img src={imageSrc} alt="Description of the image" className="rounded-sm shadow-2xl w-full h-full" />
+    </div>
   </div>
 </div>
 
