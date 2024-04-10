1d4df00bf90cba4ae213cea524d67951c618a571
nandu44-code <nanduganeshnair4444@gmail.com>
2024-04-10 18:05:47 +0530

    UI changes at ListUsers

--- Frontend/src/components/ListUsers.jsx
+++ Frontend/src/components/ListUsers.jsx
@@ -6,7 +6,7 @@ function ListUsers() {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
-  let id = 0
+  let id = 0;
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -22,14 +22,16 @@ function ListUsers() {
       }
     };
 
-    fetchUsers(); 
+    fetchUsers();
   }, [page]);
 
   const blockOrUnblockUser = async (id, isBlocked) => {
     try {
-      const response = await api.patch(`users/${id}/`, { is_active: !isBlocked });
+      const response = await api.patch(`users/${id}/`, {
+        is_active: !isBlocked,
+      });
       if (response.status === 200) {
-        const updatedUsers = users.map(user => {
+        const updatedUsers = users.map((user) => {
           if (user.id === id) {
             return { ...user, is_active: !isBlocked };
           }
@@ -47,47 +49,88 @@ function ListUsers() {
   };
 
   return (
-    <div className="ml-56 mt-20">
-      <h1 className="text-2xl font-medium text-red-400 mb-10">Users</h1>
+    <div className="mx-10 mt-20">
       <div className="overflow-x-auto">
-        <table className="table-auto border-collapse">
-          <thead>
-            <tr>
-              <th className="px-4 py-5">ID</th>
-              <th className="px-20 py-5">Username</th>
-              <th className="px-20 py-5">First Name</th>
-              <th className="px-20 py-5">Last Name</th>
-              <th className="px-20 py-5">Email</th>
-              <th className="px-20 py-5">Actions</th>
+      <input
+        type="search"
+        placeholder="search..."
+        className="rounded-full 2xl:w-96 w-56 placeholder-indigo-950 placeholder:font-bold px-4 py-2 mb-5 focus:bg-white bg-stone-200"
+        onChange={''}
+        value={''}
+      /> 
+        <table className="table-auto border-collapse hover:table-fixed bg-gray-700">
+          <caption class="caption-top mb-4 text-2xl font-bold text-gray-600">
+            Users
+          </caption>
+          <thead className="rounded-lg">
+            <tr className=" bg-gray-300 divide-x-2">
+              <th className="px-4 py-5 text-gray-600">SI.NO</th>
+              <th className="px-20 py-5 text-gray-600">Username</th>
+              <th className="px-20 py-5 text-gray-600">First Name</th>
+              <th className="px-20 py-5 text-gray-600">Last Name</th>
+              <th className="px-20 py-5 text-gray-600">Email</th>
+              <th className="px-20 py-5 text-gray-600">Actions</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => (
               // user.is_student ? (
-                <tr key={index}>
-                  <td className="border border-gray-400 px-4 py-2">{++id}</td>
-                  <td className="border border-gray-400 px-4 py-2">{user.username}</td>
-                  <td className="border border-gray-400 px-4 py-2">{user.first_name}</td>
-                  <td className="border border-gray-400 px-4 py-2">{user.last_name}</td>
-                  <td className="border border-gray-400 px-4 py-2">{user.email}</td>
-                  <td className="border border-gray-400 px-4 py-2">
-                    
-                  {user.is_active ? <button className='bg-red-700 p-2 m-2 font-bold rounded text-white' onClick={() => blockOrUnblockUser(user.id, user.is_active)}
-                    >block </button>: <button
-                    className='bg-green-400 p-2 m-2'
-                    onClick={() => blockOrUnblockUser(user.id, user.is_active)}
-                  >unblock</button>}
-                    
-                  </td>
-                </tr>
+              <tr key={index} className="hover:bg-gray-600 cursor-pointer">
+                <td className="border border-gray-400 text-white px-4 py-2">{++id}</td>
+                <td className="border border-gray-400 text-white px-4 py-2">
+                  {user.username}
+                </td>
+                <td className="border border-gray-400 text-white px-4 py-2 text-lg">
+                  {user.first_name}
+                </td>
+                <td className="border border-gray-400 text-white px-4 py-2 text-lg">
+                  {user.last_name}
+                </td>
+                <td className="border border-gray-400 text-white px-4 py-2">
+                  {user.email}
+                </td>
+                <td className="border border-gray-400 text-white px-4 py-2">
+                  {user.is_active ? (
+                    <button
+                      className="bg-red-500 px-4 py-2 m-2 font-bold rounded text-white"
+                      onClick={() =>
+                        blockOrUnblockUser(user.id, user.is_active)
+                      }
+                    >
+                     Block
+                    </button>
+                  ) : (
+                    <button
+                    className="bg-green-500 px-4 py-2 m-2 font-bold rounded text-white"
+                      onClick={() =>
+                        blockOrUnblockUser(user.id, user.is_active)
+                      }
+                    >
+                      Unblock
+                    </button>
+                  )}
+                </td>
+              </tr>
               // ) : null
             ))}
           </tbody>
         </table>
       </div>
       <div className="mt-10">
-        <button onClick={() => handlePageChange(page - 1)} disabled={page === 1} className="bg-sky-700 px-4 py-2 mx-4 text-white rounded-md">Previous</button>
-        <button onClick={() => handlePageChange(page + 1)} disabled={page === totalPages} className="bg-sky-700 px-4 py-2 mx-4 text-white rounded-md">Next</button>
+        <button
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
+          className="bg-indigo-800 px-4 py-2 mx-4 text-white rounded-md cursor-pointer hover:bg-indigo-900"
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page === totalPages}
+          className="bg-indigo-800 px-4 py-2 mx-4 text-white rounded-md cursor-pointer hover:bg-indigo-900"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
