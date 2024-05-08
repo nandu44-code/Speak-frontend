e86503949fd6a1d8e6a4fe24e3c75fa42b925141
nandu44-code <nanduganeshnair4444@gmail.com>
2024-05-08 15:05:16 +0530

    Add extra button at paymentSuccess page

--- Frontend/src/components/student/PaymentSuccess.jsx
+++ Frontend/src/components/student/PaymentSuccess.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Confetti from "react-confetti";
 import { BsCheck2Circle } from "react-icons/bs";
-
+import { Link } from "react-router-dom";
 const PaymentSuccess = () => {
   return (
-<div className="flex flex-col h-full justify-center items-center mt-32">
+<div className="flex flex-col h-full justify-center items-center mt-44">
       {/* Payment successful message */}
-      <div className="text-center shadow-2xl shadow-green-800 p-8 rounded-lg h-96">
+      <div className="text-center shadow-md shadow-black p-8 rounded-lg h-96">
         <h1 className="text-4xl font-bold text-indigo-800 mb-4 animate-bounce">
           Payment Successful!
         </h1>
       <BsCheck2Circle size={100} color={'green'} className="ml-40 mt-16 animate-bounce"/>
-      <p className="text-lg font-extralight text-indigo-700">Thank you for booking the slots Happy learning for you.</p>
+      <p className="text-lg font-normal text-gray-700">Thank you for booking the slots Happy learning for you.</p>
       </div>      
       <div className="mt-8">
         <Confetti
@@ -22,6 +22,9 @@ const PaymentSuccess = () => {
           gravity={0.5} 
           wind={0} 
         />
+        
+        <Link to ='/student/findTutors/'
+         className="bg-indigo-800 text-white px-3 py-4 rounded-md hover:shadow-none shadow-sm shadow-black">Go back to FindTutor's page</Link>
       </div>
     </div>
   );
