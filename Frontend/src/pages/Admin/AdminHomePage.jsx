87217fc178372241e70fa49c2c18300aa05173cf
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-01 01:56:22 +0530

    Admin dashboard implimented

--- Frontend/src/pages/Admin/AdminHomePage.jsx
+++ Frontend/src/pages/Admin/AdminHomePage.jsx
@@ -1,15 +1,110 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import SideBar from '../../components/SideBar'
-
+import React, { useEffect, useState } from "react";
+import SideBar from "../../components/SideBar";
+import { Chart as ChartJS } from "chart.js/auto"; // Import necessary chart.js modules
+import { Bar } from "react-chartjs-2";
+import api from "../../services/Axios";
 
 function AdminHomePage() {
+
+  const [users,setUsers] = useState('')
+  const [tutors,setTutors] = useState('')
+  const [students,setStudents] = useState('')
+  const [loading,setLoading] = useState(false)
+  useEffect(() => {
+    const fetchUsersCount = async () => {
+      setLoading(true)
+      try {
+        const response = await api.get('/user-count/');
+        console.log(response, 'users');
+        setUsers(response.data.total_users)
+        setStudents(response.data.total_students)
+        setTutors(response.data.total_tutors)
+      } catch (error) {
+        console.error('Error fetching user count:', error);
+      }
+    };
+
+    fetchUsersCount();
+    setLoading(false)
+  }, []);
+
   return (
-    <div>
-      {/* <Navbar/> */}
-      <SideBar/>
+    <div className="flex flex-row">
+      <SideBar />
+
+      <div className="flex flex-col w-full">
+        <div className="flex justify-center items-center w-full my-10">
+          <p className="font-bold text-3xl text-indigo-600">Admin Dashboard</p>
+        </div>
+        <div className="flex justify-around">
+          <div className="w-44 h-44 shadow-sm shadow-gray-500 rounded-md bg-rose-300">
+            <p className="pt-10 text-red font-normal">Total users</p>
+{     loading ?<p className="text-4xl mt-5 text-red-700 font-bold">{users}</p>:<p className="text-4xl mt-5 text-red-700 font-bold">{users}</p>
+}          </div>
+          <div className="w-44 h-44 shadow-sm shadow-gray-500 rounded-md bg-sky-300">
+            <p className="pt-10 text-red font-normal">Total Tutors</p>
+{   loading?<p>loading...</p> : <p className="text-4xl mt-5 text-red-700 font-bold">{tutors}</p>
+}          </div>
+          <div className="w-44 h-44 shadow-sm shadow-gray-500 rounded-md bg-stone-300">
+            <p className="pt-10 text-red font-normal">Total Students</p>
+{           loading ?<p>loading...</p> : <p className="text-4xl mt-5 text-red-700 font-bold">{students}</p>
+}          </div>
+        </div>
+        <div className="flex justify-center items-center mt-44 w-full">
+          <div className="mx-10 my-10">
+            <Bar
+              data={{
+                labels: ["Pending", "confirmed", "completed"],
+                datasets: [
+                  {
+                    label: "Booking",
+                    data: [200, 300, 500],
+                    backgroundColor: ["rgba(79, 12, 200,0.6)"],
+                    borderColor: ["rgba(75, 192, 192, 1)"],
+                    borderWidth: 2,
+                  },
+                ],
+              }}
+              options={{
+                scales: {
+                  y: {
+                    beginAtZero: true,
+                  },
+                },
+              }}
+              height={320}
+              className="w-full h-fit"
+            />
+          </div>
+
+          {/* <div className="mx-10 my-10" style={{ height: '320px', width: '480px' }}>
+            <Bar
+              data={{
+                labels: ["A", "B", "C"],
+                datasets: [
+                  {
+                    label: "Revenue",
+                    data: [200, 300, 500],
+                    backgroundColor: ["rgba(70, 7, 20,0.6)"],
+                    borderColor: ["rgba(75, 192, 192, 1)"],
+                    borderWidth: 2,
+                  },
+                ],
+              }}
+              options={{
+                scales: {
+                  y: {
+                    beginAtZero: true,
+                  },
+                },
+              }}
+              height={320}
+            />
+          </div> */}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AdminHomePage
+export default AdminHomePage;
--- Frontend/src/pages/Student/TutorDetailsPage.jsx
+++ Frontend/src/pages/Student/TutorDetailsPage.jsx
@@ -1,8 +1,6 @@
-import React, { useDebugValue, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import api from "../../services/Axios";
-import { useDispatch } from "react-redux";
-import SingleSlot from "../../components/tutor/SingleSlot";
 import Navbar from "../../components/Navbar";
 import { GiTeacher } from "react-icons/gi";
 import { FaUser } from "react-icons/fa";
@@ -10,20 +8,11 @@ import { FcAbout } from "react-icons/fc";
 import { FaLanguage } from "react-icons/fa6";
 import { MdOutlinePlace } from "react-icons/md";
 
-// import { AiOutlineMessage } from "react-icons/ai";
-// import { RiUserFollowLine } from "react-icons/ri";
-// import { BiHide } from "react-icons/bi";
-// import { GrSchedule } from "react-icons/gr";
-
 function TutorSideDetailsPage() {
   const { id } = useParams();
   console.log(id);
   const [userData, setUserData] = useState(null);
-  // const currentdate = new Date();
-  const [selectedDate, setSelectedDate] = useState(
-    new Date().toISOString().split("T")[0]
-  );
-  const [user, setUser] = useState("");
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split("T")[0]);
   const [slots, setSlots] = useState([]);
 
   const handleChange = (e) => {
@@ -42,7 +31,6 @@ function TutorSideDetailsPage() {
       console.log(request.data);
       const response = request.data.results;
       setSlots(response);
-      // console.log(slots)
     } catch (error) {
       console.error("Error searching slots:", error);
       toast.error("Error searching slots");
@@ -59,7 +47,6 @@ function TutorSideDetailsPage() {
         const response = await api.get(`user/${id}/`);
         console.log(response.data);
         setUserData(response.data);
-        setUser(id);
       } catch (error) {
         console.error();
       }
@@ -89,16 +76,15 @@ function TutorSideDetailsPage() {
         </div>
 
         <div className="flex w-1/2 justify-center my-5 sm:flex-row md:flex-col lg:flex-col xl:flex-row 2xl:flex-row">
-          <button className="flex bg-violet-700 text-white px-5 py-2 m-3 rounded-md font-semibold hover:bg-indigo-700 duration-1000">
+          {/* <button className="flex bg-violet-700 text-white px-5 py-2 m-3 rounded-md font-semibold hover:bg-indigo-700 duration-1000">
             Follow
-          </button>
+          </button> */}
           <a href="#slots" className="">
             <button className="flex bg-blue-600 text-white px-10 py-2 m-3 rounded-md font-semibold hover:bg-indigo-700 duration-1000">
               Schedule
             </button>
           </a>
           <button className="flex bg-sky-500 text-white px-10 py-2 m-3 rounded-md font-semibold hover:bg-indigo-700 duration-1000">
-            {" "}
             Message
           </button>
         </div>
@@ -107,17 +93,17 @@ function TutorSideDetailsPage() {
           {userData && (
             <div>
               <div className="flex flex-row gap-10">
-              <div className="flex flex-col">
-                <p className="font-normal flex justify-start text-md text-gray-800 mb-4 px-2">
-                  <span className="mr-4 py-1">
-                    <FaUser color="indigo" />
-                  </span>
-                  Username
-                </p>
-                <p className="font-semibold mb-4 flex justify-start text-lg text-gray-800 bg-stone-200 px-4 py-2 rounded-md w-fit">
-                  {userData.username}
-                </p>
-              </div>
+                <div className="flex flex-col">
+                  <p className="font-normal flex justify-start text-md text-gray-800 mb-4 px-2">
+                    <span className="mr-4 py-1">
+                      <FaUser color="indigo" />
+                    </span>
+                    Username
+                  </p>
+                  <p className="font-semibold mb-4 flex justify-start text-lg text-gray-800 bg-stone-200 px-4 py-2 rounded-md w-fit">
+                    {userData.username}
+                  </p>
+                </div>
                 <div>
                   <p className="font-normal flex justify-start text-md text-gray-800 mb-4 px-2">
                     <span className="mr-4 py-1">
@@ -132,7 +118,7 @@ function TutorSideDetailsPage() {
                 <div>
                   <p className="font-normal flex justify-start text-md text-gray-800 mb-4 px-2">
                     <span className="mr-4 py-1">
-                      <MdOutlinePlace color="green" size={18}/>
+                      <MdOutlinePlace color="green" size={18} />
                     </span>
                     State
                   </p>
@@ -152,9 +138,28 @@ function TutorSideDetailsPage() {
                   </p>
                 </div>
               </div>
-              
+
+              <div className="mt-6">
+                <p className="font-normal flex justify-start text-md text-gray-800 mb-4 px-2">
+                  <span className="mr-4 py-1">
+                    <FaUser color="indigo" />
+                  </span>
+                  Student Preferences
+                </p>
+                <div className="flex flex-wrap gap-2">
+                  {userData.tutor.student_preferences.map((preference, index) => (
+                    <p
+                      key={index}
+                      className="font-semibold text-lg text-gray-800 bg-stone-200 px-4 py-2 rounded-md"
+                    >
+                      {preference}
+                    </p>
+                  ))}
+                </div>
+              </div>
             </div>
           )}
+
           {userData && (
             <div>
               <div className="p-4">
@@ -188,10 +193,7 @@ function TutorSideDetailsPage() {
           className="w-1/2 mx-4  flex justify-center items-center bg-stone-200 h-28 rounded-2xl px-4 py-2 mb-20"
           id="slots"
         >
-          {/* <h2 className="text-xl font-bold mb-4">Search Slots by Date</h2> */}
           <form onSubmit={handleSubmit} className="flex items-center">
-            {" "}
-            {/* Added flex and items-center class */}
             <div className="mb-4">
               <label
                 htmlFor="selectedDate"
@@ -206,7 +208,6 @@ function TutorSideDetailsPage() {
                 value={selectedDate}
                 onChange={handleChange}
                 className="border border-gray-300 rounded-md p-2 w-full"
-                default={selectedDate}
                 required
               />
             </div>
