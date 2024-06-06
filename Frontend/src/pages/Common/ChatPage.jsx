2e6d9c12d93c8c292d29846a0c13bccdc7ba661d
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-06 16:23:30 +0530

    Websocket Integration completed

--- Frontend/src/pages/Common/ChatPage.jsx
+++ Frontend/src/pages/Common/ChatPage.jsx
@@ -2,24 +2,36 @@ import React, { useEffect, useState } from "react";
 import UserList from "../../components/chat/Userlist";
 import ChatWindow from "../../components/chat/ChatWindow";
 import Navbar from "../../components/Navbar";
-import jwtDecode from "jwt-decode";  // Remove curly braces around jwtDecode
+import {jwtDecode} from "jwt-decode";  
 import api from "../../services/Axios";
 
 function ChatPage() {
-  const [users, setUsers] = useState([]);  // Initialize as an empty array
-  const [selectedUser, setSelectedUser] = useState(null);  // Initialize as null
-
-  const [messages] = useState([
-    { from: 1, content: "Hello from User 1" },
-    { from: 2, content: "Hi from User 2" },
-    { from: 2, content: "Hi from User 2" },
-    // Add more messages here
-  ]);
-
-  console.log(selectedUser, 'selectedUser');
+  const [users, setUsers] = useState([]);  
+  const [selectedUser, setSelectedUser] = useState(null); 
+  const [socket,setSocket] = useState(null)
+  const [messages] = useState([]);
 
   const access = jwtDecode(localStorage.getItem('accessToken'));
   const sender_id = access.user;
+
+  const handleUserSelect = async (user) => {
+    setSelectedUser(user)
+    console.log(user,'this is the user yo')
+    const receiver_id = user.id
+    const ws = new WebSocket(`ws://localhost:8000/ws/chat/${sender_id}/${receiver_id}/`)
+    setSocket(ws)
+
+    ws.onopen = () => { 
+        console.log('websocket connected') 
+    }
+
+    ws.onmessage = (event) => {
+        console.log("Message received:", event.data);
+      };
+    ws.onclose = () => {
+        console.log("WebSocket disconnected");
+      };
+  }
 
   useEffect(() => {
     const fetch_receivers = async () => {
@@ -37,7 +49,7 @@ function ChatPage() {
         <Navbar />
       </div>
       <div className="flex flex-1">
-        <UserList users={users} onSelectUser={setSelectedUser} />
+        <UserList users={users} onSelectUser={handleUserSelect} />
         <ChatWindow
           selectedUser={selectedUser}
           messages={selectedUser ? messages.filter((msg) => msg.from === selectedUser.id) : []}
