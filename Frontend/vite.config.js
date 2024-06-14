e4bd86c18e95dc18a9f3b70e35d21e929b35bea9
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-15 00:47:32 +0530

    vite.config.js updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['uuid'], // Specify external dependencies here
+    },
+  },
+});
