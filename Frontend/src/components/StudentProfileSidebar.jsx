834d5389cdc13f22b5608d4b33f57a07d722f1c2
nandu44-code <nanduganeshnair4444@gmail.com>
2024-06-24 15:48:32 +0530

    StudentProfileSidebar UI changed

--- Frontend/src/components/StudentProfileSidebar.jsx
+++ Frontend/src/components/StudentProfileSidebar.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BsHouseDoor, BsPeople } from 'react-icons/bs';
-import { IoMdSchool } from 'react-icons/io';
-import { HiOutlineDocumentReport, HiOutlineClipboardList } from 'react-icons/hi';
+import { HiOutlineClipboardList } from 'react-icons/hi';
 
 const StudentProfileSidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,19 +11,19 @@ const StudentProfileSidebar = () => {
   };
 
   return (
-    <nav className={`flex justify-items-end bg-stone-200 pt-10 mt-64 ml-40 text-indigo-900 font-semibold text-md h-1/2 w-1/6 p-4  ${isOpen ? 'md:block' : 'hidden md:block'} rounded-lg`}>
+    <nav className={`fixed top-0 left-0 h-full bg-stone-200 text-indigo-900 font-semibold text-md p-4 ${isOpen ? 'block' : 'hidden md:block'} rounded-lg`}>
       <ul>
         <li className="hover:scale-110 mb-4">
           <Link to="/student/profile/" className="block py-2 bg-zinc-300 hover:bg-gray-200 rounded-lg" onClick={toggleSidebar}>
             <BsHouseDoor size={20} className="inline mr-2 hover:text-blue-800" />
-           Basic Details
+            Basic Details
           </Link>
         </li>
         <li className="hover:scale-110 mb-4">
           <Link to="/student/wallet/" className="block py-2 bg-zinc-300 hover:bg-gray-200 rounded-lg" onClick={toggleSidebar}>
             <BsHouseDoor size={20} className="inline mr-2 hover:text-blue-800" />
-          Wallet          
-        </Link>
+            Wallet
+          </Link>
         </li>
         <li className="hover:scale-105 mb-4">
           <Link to="/student/changePassword/" className="block py-2 bg-zinc-300 hover:bg-gray-200 rounded-lg" onClick={toggleSidebar}>
